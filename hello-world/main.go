d5a95e1f3b58e3b8018c47db64b4e11ff4a5346a
Yusuf Erdem <81750669+yusuf-erdem@users.noreply.github.com>
2022-11-15 09:02:00 +0300

    Add files via upload

---
+++ hello-world/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+var (
+	// DefaultHTTPGetAddress Default Address
+	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
+
+	// ErrNoIP No IP found in response
+	ErrNoIP = errors.New("No IP in HTTP response")
+
+	// ErrNon200Response non 200 status code in response
+	ErrNon200Response = errors.New("Non 200 Response found")
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	resp, err := http.Get(DefaultHTTPGetAddress)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if resp.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	}
+
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if len(ip) == 0 {
+		return events.APIGatewayProxyResponse{}, ErrNoIP
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("Hello Local, %v", string(ip)),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	os.Setenv("AWS_LAMBDA_RUNTIME_API", "127.0.0.1:9100")
+	fmt.Println("Write env: " + os.Getenv("AWS_LAMBDA_RUNTIME_API"))
+	lambda.Start(handler)
+}
